77c1de4875c39004c3bdc4c9ae5064d6a9751026
Sonish <sonishn213@gmail.com>
2022-07-29 22:18:22 +0530

    navbar complete

---
+++ auth/login.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$titleofpage = "login";
+include '../includes/header.php';
+
+?>
+<main>
+    <h1>login</h1>
+</main>
+<?php
+include '../includes/footer.php';
+?>
--- index.php
+++ index.php
@@ -38,22 +38,24 @@ if (isset($_POST['updatetodo'])) {
 $todos = $todosObj->getAllTodo();
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<?php
+$titleofpage = "create Todo";
+include 'includes/header.php'
+?>
 
-<body>
+<!----->
+<!-- Render start -->
+
+<main>
     <h1>Todo App</h1>
+    <!-- form to add todo-->
     <form method="post" action=<?php echo $_SERVER['PHP_SELF'] ?>>
         <input type="text" name="todo">
         <input type="submit" name="submitform" value="ADD">
     </form>
+
+    <!-- display todo-->
     <ul>
         <?php
         if (!empty($todos)) :
@@ -62,27 +64,27 @@ $todos = $todosObj->getAllTodo();
                 $id = $todo['id'];
         ?>
                 <li>
+                    <!-- display todo-->
                     <?php echo $todoname ?>
                     <div style="display:flex;">
+                        <!-- form to delete todo-->
                         <form method="post" action=<?php echo $_SERVER['PHP_SELF'] ?>>
                             <input type="text" name="todoid" value=<?php echo $id ?> hidden>
                             <input type="submit" name="deletetodo" value="DELETE">
                         </form>
+                        <!-- form to update todo-->
                         <form method='post' action=<?php echo $_SERVER['PHP_SELF'] ?>>
                             <input type="text" name="todoid" value=<?php echo $id ?> hidden>
                             <input type="text" name="updateValue">
                             <input type="submit" name="updatetodo" value="UPDATE">
                         </form>
                     </div>
-
                 </li>
         <?php
             endforeach;
         endif;
         ?>
-
-
     </ul>
-</body>
-
-</html>
+    <!-- end of display todo-->
+</main>
+<?php include 'includes/footer.php' ?>
